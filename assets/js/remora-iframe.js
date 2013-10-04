acc7f344b094e92b2536b088bb3212cac00150e6
Christopher Anderton <chris@tikidream.com>
2013-10-04 13:37:42 -0400

    Added importing of parent styles

--- assets/js/remora-iframe.js
+++ assets/js/remora-iframe.js
@@ -1,12 +1,21 @@
 jQuery(function($) {
 	$( document ).ready(function() {
+		// Get styles for inserting into the frame later
+		var parent_styles = $('link[rel=stylesheet]').clone();
+
 		// Add all styles into iframes
 		$("link[type='text/css']").clone().prependTo($("iframe.remora-frame").contents().find("head"));
 		$('iframe.remora-frame').load(function() { 
-			console.log(this);
-			//console.log($(this).contents().prop('scrollHeight'));
-			console.log( $(this).contents().height() );
+
+			// Add styles
+			$(this).contents().find('head').append(parent_styles);
+			//$(this).contents().prepend('<h1>wha?</h1>');
+
+			// Now that we're done potentially changing the height of things, 
+			// change height of frame to match height of contents
 			$(this).height( $(this).contents().height() );
+
+
 		});
 	});
 	
